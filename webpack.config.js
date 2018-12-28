f311d598349ef9a5361cb22f85d8efc7b8091276
marcmaniez <marc.maniez@gmail.com>
2018-12-27 23:06:09 -0800

    Compile scss files into a bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,58 @@ const nodeExternals = require('webpack-node-externals')
 const nodeEnv = (process.env.NODE_ENV || 'development')
 const isProduction = (nodeEnv === 'production')
 
-module.exports = {
-  devtool: 'inline-source-map',
-  entry: './app/server.ts',
-  externals: [nodeExternals()],
-  mode: isProduction ? 'production' : 'development',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+module.exports = [
+  {
+    devtool: 'inline-source-map',
+    entry: './app/server.ts',
+    externals: [nodeExternals()],
+    mode: isProduction ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    output: {
+      filename: 'server-bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+      extensions: [ '.ts', '.js' ]
+    },
+    target: 'node',
   },
-  output: {
-    filename: 'server-bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  {
+    entry: './views/index.scss',
+    output: {
+      filename: 'bundle.min.css',
+      path: path.resolve(__dirname, 'dist')
+    },
+    mode: isProduction ? 'production' : 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ]
+        },
+      ],
+    },
   },
-  resolve: {
-    extensions: [ '.ts', '.js' ]
-  },
-  target: 'node',
-};
+]
+
