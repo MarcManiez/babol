2f14e6ca69f0bc08565c229b1e1768ea96ef3966
marcmaniez <marc.maniez@gmail.com>
2018-12-29 21:06:01 -0800

    Split server and template-specific options into their own objects.

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,82 @@
 require('dotenv').config()
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
+const merge = require('webpack-merge')
 const nodeExternals = require('webpack-node-externals')
 
 const nodeEnv = process.env.NODE_ENV || 'development'
 const isProduction = nodeEnv === 'production'
 const mode = isProduction ? 'production' : 'development'
 
-module.exports = [
-  {
-    devtool: 'inline-source-map',
-    entry: {
-      server: './app/server.ts',
-    },
-    externals: [nodeExternals()],
-    mode,
-    module: {
-      rules: [
-        {
-          test: /\.ts?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    output: {
-      filename: '[name]-bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    target: 'node',
-  },
-  {
-    devtool: 'inline-source-map',
-    entry: {
-      client: './app/views/client-app.ts',
-    },
-    mode,
-    module: {
-      rules: [
-        {
-          test: /\.ts?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-        {
-          test: /\.(css|scss)$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(gif|jpg|png|svg|eot|woff)$/,
-          use: {
-            loader: 'file-loader',
-          },
-        },
-      ],
-    },
-    output: {
-      filename: '[name]-bundle.js',
-      path: path.resolve('dist'),
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: '[name]-bundle.css',
-      }),
+const serverAndTemplatesOptions = {
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
-    resolve: {
-      extensions: ['.js', '.scss', '.ts'],
-    },
   },
-]
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+}
+
+const serverConfiguration = merge(serverAndTemplatesOptions, {
+  entry: {
+    server: './app/server.ts',
+  },
+  externals: [nodeExternals()],
+  target: 'node',
+})
+
+const templatesConfiguration = merge(serverAndTemplatesOptions, {
+  entry: {
+    client: './app/views/client-app.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(gif|jpg|png|svg|eot|woff)$/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]-bundle.css',
+    }),
+  ],
+  resolve: {
+    extensions: ['.scss'],
+  },
+})
+
+module.exports = [serverConfiguration, templatesConfiguration]
