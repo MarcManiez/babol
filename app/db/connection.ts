6801c77f81763f76e525eafac69bd88ce2581b94
marcmaniez <marc.maniez@gmail.com>
2019-01-05 10:09:17 -0800

    Construct database URL for production.

--- app/db/connection.ts
+++ app/db/connection.ts
@@ -3,9 +3,25 @@ import { createConnection } from 'typeorm'
 import SpotifyLinkCollection from '../models/SpotifyLinkCollection'
 import TestModel from '../models/TestModel'
 
+function url() {
+  // TODO: Potentially handle test environment separately
+  if (process.env.NODE_ENV !== 'production') {
+    return process.env.DB_URL
+  } else {
+    const {
+      RDS_USERNAME,
+      RDS_PASSWORD,
+      RDS_HOSTNAME,
+      RDS_PORT,
+      RDS_DB_NAME,
+    } = process.env
+    return `postgresql://${RDS_USERNAME}:${RDS_PASSWORD}@${RDS_HOSTNAME}:${RDS_PORT}/${RDS_DB_NAME}`
+  }
+}
+
 export default createConnection({
   type: 'postgres',
-  url: process.env.DB_URL,
+  url: url(),
   entities: [SpotifyLinkCollection, TestModel],
   synchronize: process.env.NODE_ENV === 'development',
   logging:
