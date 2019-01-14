fb247d14092f7ebbfed1f5886a76a55e61f82928
marcmaniez <marc.maniez@gmail.com>
2019-01-13 23:38:05 -0800

    Close connection when we're done with it.

--- app/routes.ts
+++ app/routes.ts
@@ -1,16 +1,17 @@
 import { Express } from 'express-serve-static-core'
 
-import connection from './db/connection'
+import createConnection from './db/connection'
 import TestModel from './models/TestModel'
 
 export default function routes(app: Express) {
   app.get('/', async (req, res) => {
     const testModel = new TestModel()
     testModel.haha = 0
-    const connectionInstance = await connection
+    const connectionInstance = await createConnection()
     const testRepository = connectionInstance.getRepository(TestModel)
     await testRepository.save(testModel)
     const allTestModels = await testRepository.find()
+    connectionInstance.close()
     res.render('index', { count: allTestModels.length })
   })
 }
