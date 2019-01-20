d70310db61a58376f3d82532a2659d4badba969c
marcmaniez <marc.maniez@gmail.com>
2019-01-20 12:18:34 -0800

    Use connection pool.

--- app/routes.ts
+++ app/routes.ts
@@ -1,17 +1,16 @@
 import { Express } from 'express-serve-static-core'
+import { getConnection } from 'typeorm'
 
-import createConnection from './db/connection'
 import TestModel from './models/TestModel'
 
 export default function routes(app: Express) {
   app.get('/', async (req, res) => {
     const testModel = new TestModel()
     testModel.haha = 0
-    const connectionInstance = await createConnection()
+    const connectionInstance = await getConnection()
     const testRepository = connectionInstance.getRepository(TestModel)
     await testRepository.save(testModel)
     const allTestModels = await testRepository.find()
-    connectionInstance.close()
     res.render('index', { count: allTestModels.length })
   })
 }
--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,7 @@
 import { config } from 'dotenv'
 import * as express from 'express'
 
+import createConnection from './db/connection'
 import routes from './routes'
 
 config()
@@ -12,4 +13,7 @@ app.set('view engine', 'pug')
 
 routes(app)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+createConnection().then(_connectionPool => {
+  console.log('Connection pool instantiated')
+})
+app.listen(port, () => console.log(`Listening on port ${port}!`))
