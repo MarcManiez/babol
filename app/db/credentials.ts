4138133c386c9366be12b3b1eccbe7c011b6f23c
marcmaniez <marc.maniez@gmail.com>
2019-01-20 12:18:49 -0800

    Setup database credentials for test environment.

--- app/db/credentials.ts
+++ app/db/credentials.ts
@@ -5,17 +5,27 @@ const { config } = require('dotenv')
 config()
 
 module.exports = function credentials() {
-  let host = process.env.DB_HOST
-  let port = process.env.DB_PORT
-  let username = process.env.DB_USERNAME
-  let password = process.env.DB_PASSWORD
-  let database = process.env.DB_NAME
+  let host
+  let port
+  let username
+  let password
+  let database
   if (process.env.NODE_ENV === 'production') {
     host = process.env.RDS_HOSTNAME
     port = process.env.RDS_PORT
     username = process.env.RDS_USERNAME
     password = process.env.RDS_PASSWORD
     database = process.env.RDS_DB_NAME
+  } else if (process.env.NODE_ENV === 'test') {
+    host = 'localhost'
+    port = 5432
+    database = 'babol_test'
+  } else {
+    host = process.env.DB_HOST
+    port = process.env.DB_PORT
+    username = process.env.DB_USERNAME
+    password = process.env.DB_PASSWORD
+    database = process.env.DB_NAME
   }
   return { host, port, username, password, database }
 }
