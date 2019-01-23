f0256999288d4cf220c6fcd05aaf579a0cd14d9c
marcmaniez <marc.maniez@gmail.com>
2019-01-22 23:35:12 -0800

    Create fixtures for links.

---
+++ __tests__/factories/links.ts
@@ -0,0 +1,28 @@
+export const appleShortUrl = 'https://itun.es/us/8FRu'
+
+export const appleAlbumUrl =
+  'https://itunes.apple.com/us/album/walk-of-fire/1254730401'
+
+export const appleArtistUrl =
+  'https://itunes.apple.com/us/artist/aaron-goldberg/id5421052'
+
+export const appleTrackUrl =
+  'https://itunes.apple.com/us/album/walk-of-fire/1254730401?i=1254730420'
+
+export const spotifyPlayUrl =
+  'https://open.spotify.com/album/6M0IAJHwQ6dFNtTkFsXCJc'
+
+export const spotifyAlbumUrl =
+  'https://open.spotify.com/album/6M0IAJHwQ6dFNtTkFsXCJc'
+
+export const spotifyArtistUrl =
+  'https://open.spotify.com/artist/0BTfBwYC5Mw5ezDg91JBma'
+
+export const spotifyTrackUrl =
+  'https://open.spotify.com/track/4JehYebiI9JE8sR8MisGVb'
+
+export const spotifyArtistUri = 'spotify:artist:6PkSULcbxFKkxdgrmPGAvn'
+
+export const spotifyAlbumUri = 'spotify:album:2XClSOjimwtkeWYPo53mHG'
+
+export const spotifyTrackUri = 'spotify:track:2wZTxjnKdOpgPLi85ItFlr'
