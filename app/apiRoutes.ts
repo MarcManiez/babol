c22901abf6f3d32a5bba7d38e29a21f402470b8b
marcmaniez <marc.maniez@gmail.com>
2019-02-19 23:39:40 -0800

    Be less greedy with import names.

--- app/apiRoutes.ts
+++ app/apiRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 
-import { post } from './controllers/linksController'
+import * as linksController from './controllers/linksController'
 import { links } from './routeHelpers'
 
 const apiRoutes = Router()
-apiRoutes.post(links, post)
+apiRoutes.post(links, linksController.post)
 
 export default apiRoutes
