f1b55b82a2aaa9245757add3d5c4f57d73754734
marcmaniez <marc.maniez@gmail.com>
2019-03-10 19:40:28 -0700

    Add type definitions for jaro-winkler.

---
+++ @types/jaro-winkler.d.ts
@@ -0,0 +1,10 @@
+declare module 'jaro-winkler' {
+  interface Options {
+    caseSensitive: boolean
+  }
+  export default function distance(
+    string1: string,
+    string2: string,
+    options: Options,
+  ): number
+}
