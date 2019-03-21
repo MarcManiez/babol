d848a0a56e0d35e8cddf52c935d0172fcb54acf8
marcmaniez <marc.maniez@gmail.com>
2019-03-20 20:12:41 -0700

    Take first pass at transforming links.

--- app/controllers/linksController.ts
+++ app/controllers/linksController.ts
@@ -4,12 +4,17 @@ import { getConnection } from 'typeorm'
 import * as AppleLinkParsing from '../domain/link_parsing/apple'
 import { detectStreamingService } from '../domain/link_parsing/general'
 import * as SpotifyLinkParsing from '../domain/link_parsing/spotify'
+import * as AppleResultParsing from '../domain/service_result_parsing/apple'
+import * as SpotifyResultParsing from '../domain/service_result_parsing/spotify'
 import { parseSlug } from '../domain/slugs'
 import {
   MissingParamsError,
   SlugParsingError,
   UnknownStreamingServiceError,
 } from '../errors'
+import * as AppleClient from '../lib/clients/apple'
+import { coreLinkPropertiesToSearchQuery } from '../lib/clients/helpers'
+import SpotifyClient from '../lib/clients/spotify'
 import AppleLinkCollection from '../models/AppleLinkCollection'
 import SpotifyLinkCollection from '../models/SpotifyLinkCollection'
 import { StreamingService } from '../types/babol'
@@ -52,9 +57,9 @@ export async function post(req: Request, res: Response) {
   try {
     const host = req.headers && req.headers.host
     const streamingService = detectStreamingService(link)
+    const connection = getConnection()
     if (streamingService === StreamingService.Apple) {
       const id = AppleLinkParsing.getId(link)
-      const connection = getConnection()
       const repository = connection.getRepository(AppleLinkCollection)
       let appleLinkCollection = await repository.findOne({ sourceId: id })
       if (!appleLinkCollection) {
@@ -71,20 +76,35 @@ export async function post(req: Request, res: Response) {
       })
     } else if (streamingService === StreamingService.Spotify) {
       const id = SpotifyLinkParsing.getId(link)
-      const connection = getConnection()
       const repository = connection.getRepository(SpotifyLinkCollection)
+      const type = SpotifyLinkParsing.getType(link)
       let spotifyLinkCollection = await repository.findOne({ sourceId: id })
       if (!spotifyLinkCollection) {
         spotifyLinkCollection = new SpotifyLinkCollection(link)
         spotifyLinkCollection = await repository.save(spotifyLinkCollection)
       }
       if (!spotifyLinkCollection.appleLink) {
-        sleep(5).then(time =>
-          console.log(`I slept for ${time} seconds, look at me!`),
+        const result = await SpotifyClient.fetch(type, id)
+        const coreLinkProperties = SpotifyResultParsing.extractCoreLinkProperties(
+          result,
         )
-        // Spin off worker to fetch song from Apple
-        // Careful with this: if for some reason the song can't ever be found,
-        // we'll be making requests forever for nothing
+        const results = await AppleClient.search(
+          coreLinkPropertiesToSearchQuery(coreLinkProperties),
+          [type],
+        )
+        const bestMatch = AppleResultParsing.extractLink(
+          results,
+          coreLinkProperties,
+        )
+        spotifyLinkCollection.appleLink = bestMatch.url
+        spotifyLinkCollection.artist = coreLinkProperties.artist
+        if (coreLinkProperties.album) {
+          spotifyLinkCollection.album = coreLinkProperties.album
+        }
+        if (coreLinkProperties.artist) {
+          spotifyLinkCollection.artist = coreLinkProperties.artist
+        }
+        await repository.save(spotifyLinkCollection)
       }
       return res.json({
         babol_link: host && host + spotifyLinkCollection.babolLinkPath(),
@@ -97,10 +117,4 @@ export async function post(req: Request, res: Response) {
       throw error
     }
   }
-}
-
-function sleep(timeInSeconds: number) {
-  return new Promise(resolve =>
-    setTimeout(() => resolve(timeInSeconds), timeInSeconds * 1000),
-  )
 }
